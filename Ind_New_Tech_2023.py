290b5f70ade661433bbbb7173903d6301a267411
luozero@126.com <luozero@126.com>
2023-07-23 00:31:21 +0000

    update for rebase

--- Ind_New_Tech_2023.py
+++ Ind_New_Tech_2023.py
@@ -1,9 +1,9 @@
 
 import sys
 import os 
-# module_path = os.path.abspath(os.path.join('..')) 
-# if module_path not in sys.path: 
-#     sys.path.append(module_path)
+module_path = os.path.abspath(os.path.join('..')) 
+if module_path not in sys.path: 
+    sys.path.append(module_path)
 
 
 import numpy as np
